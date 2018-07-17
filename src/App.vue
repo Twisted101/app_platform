d5f5dc42d922cb7736b261859c0533b9ce675e73
shenwei <759576923@qq.com>
2018-07-17 16:58:13 +0800

    1.0

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <div class="btn-group">
+      <button class="btn btn-primary">测试bootstrap样式</button>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+</style>
